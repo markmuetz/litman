d753521e719139fbe60f60670ec544079b15f6d6
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-12-04 10:28:03 +0000

    Allow display to open multiple PDFs at once.

--- litman/cmds/display.py
+++ litman/cmds/display.py
@@ -1,14 +1,15 @@
 """Dislpay a given litman item"""
 from litman.litman import ItemNotFound
 
-ARGS = [(['list_item'], {'nargs': 1, 'help': 'Display list item'})]
+ARGS = [(['list_items'], {'nargs': '+', 'help': 'Display list item'})]
 
 
 def main(litman, args):
-    try:
-        item = litman.get_item(args.list_item[0])
-    except:
-        print('Could not find item')
-        return
-    print(f'Name: {item.name}')
-    item.display()
+    for item_name in args.list_items:
+        try:
+            item = litman.get_item(item_name)
+        except ItemNotFound:
+            print('Could not find item')
+            continue
+        print(f'Name: {item.name}')
+        item.display()
